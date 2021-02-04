a4f419c275cd4175a74057ec4adbe293b5b09d98
Jigar Mehta <hello@jigarius.com>
2021-02-03 23:03:33 -0500

    Simplify FizzBuzz::generate()

--- lib/FizzBuzz.php
+++ lib/FizzBuzz.php
@@ -1,65 +1,29 @@
 <?php
 
+use FizzBuzz\Number;
+
 /**
  * Class FizzBuzz.
  */
 class FizzBuzz {
 
   /**
-   * Returns fizz buzz for a number.
+   * Return fizzbuzz from 1 to $limit.
    *
-   * @param int $number
-   *   A number.
-   *
-   * @return string
-   *   Returns one of "fizz", "buzz", "fizzbuzz" or the original number.
-   *
-   *   "fizz" if the number is divisible by 3.
-   *   "buzz" if the number is divisible by 5.
-   *   "fizzbuzz" if the number is divisible by 3 & 5.
-   *   The number, if it is not divisible by 3 or 5.
-   */
-  public static function processNumber(int $number): string {
-    $output = '';
-    if ($number % 3 === 0) {
-      $output .= 'fizz';
-    }
-    if ($number % 5 === 0) {
-      $output .= 'buzz';
-    }
-    return $output ?: (string) $number;
-  }
-
-  /**
-   * Return fizz buzz for a range of numbers.
-   *
-   * @param int $from
-   *   Range start.
-   * @param int $till
-   *   Range end.
+   * @param int $limit
+   *   A limit.
    *
    * @return array
    *   An array containing "fizzbuzz" equivalent of each number.
    */
-  public static function processRange(int $from, int $till): array {
-    return self::processArray(range($from, $till));
-  }
+  public static function generate(int $limit): array {
+    $numbers = range(1, $limit);
 
-  /**
-   * Return fizz buzz for a range of numbers.
-   *
-   * @param int[] $numbers
-   *   An array of numbers.
-   *
-   * @return array
-   *   An array containing "fizzbuzz" equivalent of each number.
-   */
-  private static function processArray(array $numbers): array {
-    $output = [];
-    foreach ($numbers as $key => $number) {
-      $output[$key] = self::processNumber($number);
+    foreach ($numbers as &$number) {
+      $number = (string) new Number($number);
     }
-    return $output;
+
+    return $numbers;
   }
 
 }
--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -8,60 +8,24 @@ use PHPUnit\Framework\TestCase;
 class FizzBuzzTest extends TestCase {
 
   /**
-   * Test "fizz" when number is divisible by 3.
-   */
-  public function testFizz() {
-    $this->assertSame(FizzBuzz::processNumber(3), 'fizz');
-    $this->assertSame(FizzBuzz::processNumber(6), 'fizz');
-    $this->assertSame(FizzBuzz::processNumber(333), 'fizz');
-  }
-
-  /**
-   * Test "buzz" when number is divisible by 5.
-   */
-  public function testBuzz() {
-    $this->assertSame(FizzBuzz::processNumber(5), 'buzz');
-    $this->assertSame(FizzBuzz::processNumber(10), 'buzz');
-    $this->assertSame(FizzBuzz::processNumber(500), 'buzz');
-  }
-
-  /**
-   * Test "fizzbuzz" when number is divisible by both 3 and 5.
-   */
-  public function testFizzBuzz() {
-    $this->assertSame(FizzBuzz::processNumber(15), 'fizzbuzz');
-    $this->assertSame(FizzBuzz::processNumber(30), 'fizzbuzz');
-    $this->assertSame(FizzBuzz::processNumber(225), 'fizzbuzz');
-  }
-
-  /**
-   * Test when number is not divisible by 3 or 5.
-   */
-  public function testUndivisible() {
-    $this->assertEquals(FizzBuzz::processNumber(2), 2);
-    $this->assertEquals(FizzBuzz::processNumber(13), 13);
-    $this->assertEquals(FizzBuzz::processNumber(1001), 1001);
-  }
-
-  /**
    * Test for number range 1 to 15.
    */
-  public function testRange() {
-    $this->assertEquals(FizzBuzz::processRange(1, 15), [
-      1,
-      2,
+  public function testGenerate() {
+    $this->assertEquals(FizzBuzz::generate(15), [
+      '1',
+      '2',
       'fizz',
-      4,
+      '4',
       'buzz',
       'fizz',
-      7,
-      8,
+      '7',
+      '8',
       'fizz',
       'buzz',
-      11,
+      '11',
       'fizz',
-      13,
-      14,
+      '13',
+      '14',
       'fizzbuzz',
     ]);
   }
